c52f8170341fa9877e3d56b676ec6bd1ea643896
xbrooke <107459849+xbrooke@users.noreply.github.com>
2023-10-15 10:24:50 +0000

    Deploying to gh-pages from  @ ee142e1449a7eb0c32bb7c958224ebdcd902add5 🚀

---
+++ assets/azjc_qy_d3.md.d7161580.lean.js
@@ -0,0 +1,1 @@
+import{_ as a,o as e,c as t,Q as o}from"./chunks/framework.865c6d19.js";const u=JSON.parse('{"title":"为什么安装第三方软件？","description":"","frontmatter":{},"headers":[],"relativePath":"azjc/qy/d3.md","filePath":"azjc/qy/d3.md"}'),c={name:"azjc/qy/d3.md"},s=o("",4),r=[s];function _(n,d,i,l,p,h){return e(),t("div",null,r)}const f=a(c,[["render",_]]);export{u as __pageData,f as default};
