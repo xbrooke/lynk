1364a9f477a718e6b5182786be01f619a0a3808d
xbrooke <554719247@qq.com>
2023-10-24 18:21:07 +0800

    创建 GiscusComment.vue

---
+++ docs/.vitepress/theme/components/GiscusComment.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <div ref="giscus"></div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    mounted() {
+      if (window.giscus) {
+        window.giscus('render', {
+          repo: 'xbrooke/lynk-giscus', // 将此处替换为你的 GitHub 仓库名称
+          mapping: 'pathname',
+        });
+      }
+    },
+  };
+  </script>
+  
